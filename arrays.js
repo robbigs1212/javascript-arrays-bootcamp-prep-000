29abdaac2f0f43d0291767dff257a854578d5613
ROB BIRMINGHAM <robbigs@gmail.com>
2018-08-01 04:07:44 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+const array[ = [1];
+var element = 'foo'
+
+function addElementToBeginningOfArray(array, element){}
+
+addElementToBeginningOfArray = [...'foo',array]
